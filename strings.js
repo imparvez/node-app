d1d7e0a4cea63397e032bcf9996b0f29730b0065
Parvez shaikh <parvezshaikhj@gmail.com>
2015-12-05 20:12:32 +0530

    Strings in nodejs

---
+++ strings.js
@@ -0,0 +1,5 @@
+var firstName = 'parvez'; //=>'string inside'
+var _lastName = 'shaikh';
+var fullName = 'My name is ' + firstName + ' ' +_lastName
+console.log(fullName);
+console.log(fullName.length);
